5177f2b8ab596c61ecf0c28ffcefaf705f3c6f85
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 19:05:17 +0500

    new changes

---
+++ banks/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView as DjangoLoginView
+from .models import  Bank,Account
+
+# Create your views here.
+class BankListView(ListView):
+    model = Bank
+    template_name = 'bank_list.html'
+    context_object_name = 'banks'
+
+
+class AccountListView(LoginRequiredMixin,ListView):
+    model = Account
+    template_name = 'account_list.html'
+    context_object_name = 'accounts'
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
+
+
+class LoginView(DjangoLoginView):
+    template_name = 'login.html'
+
+
+class UserAccountListView(LoginRequiredMixin, ListView):
+    model = Account
+    template_name = 'user_account_list.html'
+    context_object_name = 'accounts'
+
+    def get_queryset(self):
+        return Account.objects.filter(user=self.request.user)
