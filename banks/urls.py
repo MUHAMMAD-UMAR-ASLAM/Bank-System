ae6db1626ff77c90d0d7edaf6313327ae9aeaa17
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 18:54:13 +0500

    Removed files from Git cache

--- banks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BankListView, AccountListView, LoginView, UserAccountListView
-
-urlpatterns = [
-    path('banks/', BankListView.as_view(), name='bank-list'),
-    path('accounts/', AccountListView.as_view(), name='account-list'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('user-accounts/', UserAccountListView.as_view(), name='user-account-list'),
-]
