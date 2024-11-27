1e9f80cadc66d6cdcde7777b794b0bb906bb8a75
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 19:42:37 +0500

    new changes

--- banks/models.py
+++ banks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# Create your models here.
 class Bank(models.Model):
     name = models.CharField(max_length=255)
     branch = models.CharField(max_length=100)
@@ -13,8 +12,8 @@ class Bank(models.Model):
 
 
 class Account(models.Model):
-    bank = models.ForeignKey(Bank,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE, related_name='accounts')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts')
     balance = models.DecimalField(max_digits=10,decimal_places=2)
 
     def __str__(self):
