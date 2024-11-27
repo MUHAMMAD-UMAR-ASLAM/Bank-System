b1f9790aaf445f715139586b4658d1ead707a809
umar aslam <umarasalam.lhr1@gmail.com>
2024-11-27 16:32:33 +0500

    new changes

--- bank_system/settings.py
+++ bank_system/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_REDIRECT_URL = '/user-accounts/'
+
+LOGOUT_REDIRECT_URL = '/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- banks/models.py
+++ banks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 # Create your models here.
 class Bank(models.Model):
     name = models.CharField(max_length=255)
@@ -19,7 +18,8 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=10,decimal_places=2)
 
     def __str__(self):
-        return f"Account of {self.user.name} at {self.bank.name}"
+        return f"Account of {self.user.username} at {self.bank.name}"
+
 
 
 
