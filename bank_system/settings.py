149e55f5561e679187f1998c4660b8f109fd26cd
MUHAMMAD-UMAR-ASLAM <umaraslam.lhr1@gmail.com>
2024-11-28 15:56:43 +0500

    new changes

--- bank_system/settings.py
+++ bank_system/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bank_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'/ 'banks'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
